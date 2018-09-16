24ae6c10ec6f990334e5679260688d38368327f8
Eslam <eslam.abdalla93@hmail.com>
2018-09-16 16:02:31 +0200

    Sign UP

--- app/src/main/java/com/example/eslam/mashaweer/Sign_UP_Activity.java
+++ app/src/main/java/com/example/eslam/mashaweer/Sign_UP_Activity.java
@@ -5,6 +5,7 @@ import android.app.DownloadManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.ColorSpace;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -35,8 +37,11 @@ public class Sign_UP_Activity extends AppCompatActivity {
     RadioButton Radio_B_UserType ;
     Button Sign_Up;
     String SignUp_URL ="http://Mashaweer.somee.com/api/users" ;
+    ProgressBar progressBar ;
+
 
     int User_Type = 2 ;
+
 
 
     @Override
@@ -44,6 +49,8 @@ public class Sign_UP_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up_);
 
+
+        progressBar = (ProgressBar)findViewById(R.id.login_progress);
 
         First_Name = (EditText)findViewById(R.id.SignUpFirstName);
         Last_Name  = (EditText)findViewById(R.id.SignUpLastName);
@@ -82,13 +89,14 @@ public class Sign_UP_Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                progressBar.setVisibility(View.VISIBLE);
 
 
                 String Entered_FirstName;
                 String Entered_LastName;
                 String Entered_LogInName;
-                String Entered_Password;
-                String Entered_RePassword;
+                final String Entered_Password;
+                final String Entered_RePassword;
                 int Entered_UserType;
 
 
@@ -108,49 +116,91 @@ public class Sign_UP_Activity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                if (!Entered_Password.equals(Entered_RePassword)) {
-                    RePassword.setText("");
-                    RePassword.setHint("Password Not Match");
-                    RePassword.setHintTextColor(Color.parseColor("#FF890222"));
-                } else {
-                    Sign_Up.setClickable(false);
+    String CheckUserName_URL ="http://Mashaweer.somee.com/api/CheckLogInName/?userName="+Entered_LogInName+"" ;
 
 
-                    JsonObjectRequest AddNewUserRequest = new JsonObjectRequest(Request.Method.POST, SignUp_URL, User_Data
-                            , new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Toast.makeText(Sign_UP_Activity.this, "Done", Toast.LENGTH_LONG).show();
 
+                JsonObjectRequest CheckUserName = new JsonObjectRequest(Request.Method.GET, CheckUserName_URL, null
+                        , new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
 
-                            startActivity(new Intent(Sign_UP_Activity.this,Profile.class));
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                            error.printStackTrace();
-                            error.getMessage();
-
-
+                        int State = 0;
+                        try {
+                            State = response.getInt("state");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
 
 
+                        if (State == 2) {
+                            LogInName.setText("");
+                            LogInName.setHint("User Already Exist");
+                            LogInName.setHintTextColor(Color.parseColor("#FF890222"));
+                            progressBar.setVisibility(View.GONE);
+                        } else if (State == 1) {
 
 
-                    });
+                            if (!Entered_Password.equals(Entered_RePassword)) {
+                                RePassword.setText("");
+                                RePassword.setHint("Password Not Match");
+                                RePassword.setHintTextColor(Color.parseColor("#FF890222"));
+                                progressBar.setVisibility(View.GONE);
+                            } else {
+                                Sign_Up.setClickable(false);
 
-                    MySingleton.getInstance(Sign_UP_Activity.this).addToRequestQueue(AddNewUserRequest);
+
+                                JsonObjectRequest AddNewUserRequest = new JsonObjectRequest(Request.Method.POST, SignUp_URL, User_Data
+                                        , new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        Toast.makeText(Sign_UP_Activity.this, "Done", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+
+                                        startActivity(new Intent(Sign_UP_Activity.this, Profile.class));
+                                    }
+                                }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+
+                                        error.printStackTrace();
+                                        error.getMessage();
 
 
-                }
+                                    }
+
+
+                                });
+
+
+                                MySingleton.getInstance(Sign_UP_Activity.this).addToRequestQueue(AddNewUserRequest);
+
+                            }
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        error.printStackTrace();
+                        error.getMessage();
+                        Toast.makeText(Sign_UP_Activity.this, "Connection Error", Toast.LENGTH_LONG).show();
+
+
+                    }
+
+
+
+
+                });
+                MySingleton.getInstance(Sign_UP_Activity.this).addToRequestQueue(CheckUserName);
+
+
 
             }
         });
-
-
-
 
 
     }
